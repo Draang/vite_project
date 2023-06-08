ffc804db491edb8dbff3a084dc88705be26b0715
Angel Contreras Dueñas <c3po.angel@gmail.com>
2023-06-08 12:34:21 -0600

    dependencies tailwind styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="grid col-span-1">
+      <h1 className="text-4xl font-bold uppercase ">This is a header</h1>
+    </header>
+  );
+}
+/*  */
